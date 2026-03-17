ce608ea10c985f13fd9557a6d12bb44856b62ef8
morgan24jm <164976871+morgan24jm@users.noreply.github.com>
2026-03-16 18:42:19 -0600

    Update load-test.js

--- Backend/test/load-test.js
+++ Backend/test/load-test.js
@@ -12,7 +12,7 @@ export const options = {
     { duration: "10s", target: 0 },
   ],
   thresholds: {
-    http_req_duration: ["p(95)<500"],
+    http_req_duration: ["p(95)<1000"],
     tasa_exito: ["rate>0.95"],
   },
 };
